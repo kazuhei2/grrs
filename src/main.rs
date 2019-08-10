7e6e1ee648de2ae2e13a375f38b672b7f0025df0
Kazuhei Hamada <hamada.kazuhei@gmail.com>
2019-08-10 22:57:35 +0900

    Complete nicer error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
-use structopt::StructOpt;
+use failure::ResultExt;
+use exitfailure::ExitFailure;
 
-#[derive(StructOpt)]
-struct Cli {
-	pattern: String,
-	#[structopt(parse(from_os_str))]
-	path: std::path::PathBuf,
-}
-
-fn main() {
-	let args = Cli::from_args();
-	let content = std::fs::read_to_string(&args.path)
-		.expect("could not read file");
-
-	for line in content.lines() {
-		if line.contains(&args.pattern) {
-			println!("{}", line);
-		}
-	}
+fn main() -> Result<(), ExitFailure> {
+	let path = "test.txt";
+	let content = std::fs::read_to_string(path)
+		.with_context(|_| format!("could not read file `{}`", path))?;
+	println!("file content:\n{}", content);
+	Ok(())
 }
